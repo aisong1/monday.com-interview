6b407abe4e96ec754fc1b28a6544c59da56a431b
Andrew Song <andrewsong61@gmail.com>
2024-05-18 19:38:18 -0600

    Implement bare-bones front end (wip)

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import { useState, useEffect } from "react";
 import "./App.css";
 import mondaySdk from "monday-sdk-js";
 import "monday-ui-react-core/dist/main.css";
-//Explore more Monday React Components here: https://style.monday.com/
-import AttentionBox from "monday-ui-react-core/dist/AttentionBox";
+import { Button, Dropdown, TextField } from "monday-ui-react-core";
 
 // Usage of mondaySDK example, for more information visit here: https://developer.monday.com/apps/docs/introduction-to-the-sdk/
 const monday = mondaySdk();
+
+const handleStartOrderOnClick = () => { console.log('clicked!') };
 
 const App = () => {
   const [context, setContext] = useState();
@@ -23,19 +24,62 @@ const App = () => {
     });
   }, []);
 
-  //Some example what you can do with context, read more here: https://developer.monday.com/apps/docs/mondayget#requesting-context-and-settings-data
-  const attentionBoxText = `Hello, your user_id is: ${
-    context ? context.user.id : "still loading"
-  }.
-  Let's start building your amazing app, which will change the world!`;
-
   return (
     <div className="App">
-      <AttentionBox
-        title="Hello World!"
-        text={attentionBoxText}
-        type="success"
-      />
+      <div id="form-wrapper">
+        <div id="inputs">
+          <TextField title="First Name" placeholder="Enter Customer First Name" type="text" size="large">
+            Insert your input here.
+          </TextField>
+          <TextField title="Last Name" placeholder="Enter Customer Last Name" type="text" size="large">
+            Insert your input here.
+          </TextField>
+          <TextField title="Quantity" placeholder="1" type="number" size="large">
+            Insert your input here.
+          </TextField>
+        </div>
+        <div id="dropdown">
+          <Dropdown 
+            multi
+            multiline
+            clearable
+            size="large"
+            onOptionSelect={(opt) => {console.log(opt)}}
+            onOptionRemove={(opt) => {console.log(opt)}}
+            onClear={() => { console.log('options cleared') }}
+            options={[
+              {
+                label: 'Option 1',
+                value: 1
+              },
+              {
+                label: 'Option 2',
+                value: 2
+              },
+              {
+                label: 'Option 3',
+                value: 3
+              },
+              {
+                label: 'Option 4',
+                value: 4
+              },
+              {
+                label: 'Option 5',
+                value: 5
+              },
+              {
+                label: 'Option 6',
+                value: 6
+              }
+            ]}></Dropdown>
+        </div>
+        <div id="startOrderButton" size="large">
+          <Button onClick={handleStartOrderOnClick}>
+            Start Order
+          </Button>
+        </div>
+      </div>
     </div>
   );
 };
