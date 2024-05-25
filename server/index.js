d8488c0632f317f874bb3ed3131d015ac6454e66
Andrew Song <andrewsong61@gmail.com>
2024-05-25 16:37:24 -0600

    Run backend and frontend on different ports; include "Access-Control-Allow-Origin" to allow requests from port 8301

--- server/index.js
+++ server/index.js
@@ -10,12 +10,22 @@ const router = require('./routes');
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 8080;
+const serverPort = 8080;
+const appPort = 8301;
 
 app.use(bodyParser.json());
 
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', `http://localhost:${appPort}`);
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  next();
+});
+
 app.use('/api', router);
 
-app.listen(port, () => {
-  console.log(`Listening on port ${port}`);
+app.listen(serverPort, () => {
+  console.log(`Listening on port ${serverPort}`);
 });
