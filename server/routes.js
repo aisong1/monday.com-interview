53e53cbeca6eb11a49ed07094d194723e3cef9fd
Andrew Song <andrewsong61@gmail.com>
2024-05-27 12:34:14 -0600

    Update router to route requests made to orders API to properly controllers

--- server/routes.js
+++ server/routes.js
@@ -3,6 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 // Connect controller methods to their corresponding routes
+// api/fragrances
 router.get('/fragrances', controller.fragrances.get);
 
 router.post('/fragrances', controller.fragrances.post);
@@ -10,5 +11,14 @@ router.post('/fragrances', controller.fragrances.post);
 router.put('/fragrances', controller.fragrances.put);
 
 router.delete('/fragrances', controller.fragrances.delete);
+
+// api/orders
+router.get('/orders', controller.orders.get);
+
+router.post('/orders', controller.orders.post);
+
+router.put('/orders', controller.orders.put);
+
+router.delete('/orders', controller.orders.delete);
 
 module.exports = router;
